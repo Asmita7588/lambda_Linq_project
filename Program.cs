b62bf9807d5ccce24e53a15f4827088192b20156
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-14 11:50:47 +0530

    Basic Programs On lambda Linq

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using LambdaLinqProject;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Basic Lambda Linq Programing");
+
+    }
+}
